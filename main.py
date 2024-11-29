694734104a60cd206c099dd9bed3f791ee34a1e4
rrayy <rrayy25809@gmail.com>
2024-11-29 14:30:34 +0900

    Changes to be committed: js -> python 이미지 전송, UI 배치 변경 	modified:   main.py 	new file:   saved_img.png 	modified:   static/app.js 	modified:   static/styles.css 	modified:   templates/index.html

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template, request
-from _thread import *
-client_sockets = []
+from PIL import Image
+import io
+import base64
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template("index.html")
 
-#TODO: 강의 듣고 프로그램 다 만들기, css 수정 검토하기, api로 사진 분석 -> 프롬포트 도출 -> 다시 api? 검토
+def base64toimg(encode_image):
+    _, code = encode_image.split(",")
 
-@app.route("/make_idk",methods=['POST'])
-def make_idk():
-    f = request.files.get('img')
-    print(f"eawefawe{f.filename}")
+    binary = base64.b64decode(code)
+    img = Image.open(io.BytesIO(binary))
+    return img
 
+#TODO: api?로 사진 분석 -> 프롬포트 도출 -> 다시 api? 검토, js 로 이미지 받아서 python POST로 보내기
+@app.route("/make_it",methods=['POST'])
+def make_it():
+    encode_image = request.form.get('image')
+    img = base64toimg(encode_image)
+    img.save("saved_img.png")
+    
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
